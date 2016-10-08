b96b3b80b16c05c5895e9e888b41cf735b2e0f20
Dicksociety <Dsociety@protonmail.com>
2016-10-08 23:38:51 +0200

    IntegralScan up to 0.0.4

--- IntegralScan.sh
+++ IntegralScan.sh
@@ -4,41 +4,45 @@
 
 #A utiliser en root
 
+#0.0.4
+
 #Réalise de nombreux scans sur la machine cible
 
 #A utiliser avec precaution car certains scans sont facilement detectable, ceci seront notifié et vous pourrez ainsi facilement les commentez
 
 #change  mac
 #pour rentrer une foi dans la boucle
-a = '2'
-while [ $a != '1' ] || [ $a != '0' ]
+a='2'
+while [ $a == '2' ]
 do
-echo Voulez vous changez votre adresse mac (oui=1 non=0)?
-read macyn
-a = macsyn
-if [$macyn = '1']
-
+echo Voulez vous changez votre adresse mac \(oui=1 non=0\)?
+read macsyn
+a=macsyn
+if [ $macsyn = '1' ]
+then
 	 #pour rentrer une foi dans la boucle
-	 quoi = 0
+	 quoi=0
 	while [ $quoi !=  '1' ] || [ $quoi !=  '2' ] #boucle pour recomencer si c'est diferrent de 1 ou 2
 	do
-		echo 1)adresse random 
-		echo 2)choix adresse
+		echo 1\)adresse random 
+		echo 2\)choix adresse
 		read quoi0
 		
-		quoi = quoi0
+		quoi=quoi0
 	
 	
-		echo Quelle est votre interface reseau (eth0 wlan0 etc...)
+		echo Quelle est votre interface reseau \(eth0 wlan0 etc...\)
 		read interface
 	
-		if [$quoi = '1']
+		if [ $quoi = '1' ]
+		then
 			mac0 = openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'
 			ifconfig $interface down
 			ifconfig $interface hw ether $mac0
 			ifconfig $interface up
 	
-		elif [$quoi = '2']
+		elif [ $quoi = '2' ]
+		then
 			echo votre nouvelle adresse mac:
 			read mac1
 			ifconfig $interface down
@@ -49,7 +53,9 @@ if [$macyn = '1']
 		fi
 	
 	done
-elif [$macyn = '0']
+elif [ $macsyn = '0' ]
+then
+	a=$macsyn
 	echo c est risqué de ne pas changer mais c vous le chef!
 else
 	echo choix 1 ou 0 pas autre chose!!
